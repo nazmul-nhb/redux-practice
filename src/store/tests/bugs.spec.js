12abc824c94b45c6a77a6d26ff900683ea1f334d
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-01 22:26:34 +0600

    performed test using mock axios

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,6 +1,8 @@
 import { apiCallBegan } from "../api";
 import { addBug, bugAdded } from "../bugs"
 import store from "../store";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
 
 describe("bugsSlice", () => {
     // solitary test
@@ -24,7 +26,11 @@ describe("bugsSlice", () => {
     // });
     it("should handle the addBug action", async () => {
         const bug = { description: "Bug Test" };
+        const savedBug = { ...bug, id: 1 }
+        const fakeAxios = new MockAdapter(axios);
+        fakeAxios.onPost('/bugs').reply(200, savedBug);
+
         await store.dispatch(addBug(bug));
-        expect(store.getState().entities.bugs.list).toHaveLength(1);
+        expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
     })
 });
