0d69b6b4e70a66fabb9a160e483d58e8f95e7b87
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-01 21:33:21 +0600

    tested sample math function

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,3 +1,15 @@
-it("first test", () => {
+import { isEven } from "./math";
 
-});
+describe("isEven", () => {
+    it("should return true if given an even number", () => {
+        // function under test (system under test --> SUT)
+        const result = isEven(4);
+        expect(result).toEqual(true);
+    });
+
+    it("should return true if given an odd number", () => {
+        // function under test (system under test --> SUT)
+        const result = isEven(5);
+        expect(result).toEqual(false);
+    });
+})
